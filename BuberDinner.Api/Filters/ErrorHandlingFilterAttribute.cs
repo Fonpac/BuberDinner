01d40cf09777d28190ea9e7b2db31856e5e4fa82
Bernardo <bernardo.fonpac@gmail.com>
2022-08-22 17:07:31 -0300

    Error handling

---
+++ BuberDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace BuberDinner.Api.Filters;
+
+public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
+{
+    public override void OnException(ExceptionContext context)
+    {
+        var exception = context.Exception;
+
+        var problemDetails = new ProblemDetails
+        {
+            Type = "",
+            Title = "an error has ocurred while processing your request",
+            Instance = context.HttpContext.Request.Path,
+            Status = (int)HttpStatusCode.InternalServerError,
+            Detail = exception.Message
+        };
+
+        context.Result = new ObjectResult(problemDetails);
+
+        context.ExceptionHandled = true;
+    }
+}
